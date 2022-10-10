5ad38bcb885e4c7203e7e2eaa6ca6507a854acb5
toteabe <toteabe@yahoo.es>
2022-10-10 23:36:24 +0200

    Init TiendaTest

---
+++ src/org/iesvegademijas/stream/test/TiendaTest.java
@@ -0,0 +1,623 @@
+package org.iesvegademijas.stream.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static java.util.stream.Collectors.*;
+import static java.util.Comparator.*;
+
+import org.iesvegademijas.hibernate.Fabricante;
+import org.iesvegademijas.hibernate.FabricanteHome;
+import org.iesvegademijas.hibernate.Producto;
+import org.iesvegademijas.hibernate.ProductoHome;
+import org.junit.jupiter.api.Test;
+
+
+class TiendaTest {
+	
+	@Test
+	void testSkeletonFrabricante() {
+	
+		FabricanteHome fabHome = new FabricanteHome();
+		
+		try {
+			fabHome.beginTransaction();
+	
+			List<Fabricante> listFab = fabHome.findAll();
+		
+			
+			//TODO STREAMS
+			
+		
+			fabHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			fabHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+
+	@Test
+	void testSkeletonProducto() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+		
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	@Test
+	void testAllFabricante() {
+	
+		FabricanteHome fabHome = new FabricanteHome();
+		
+		try {
+			fabHome.beginTransaction();
+	
+			List<Fabricante> listFab = fabHome.findAll();
+			assertEquals(9,listFab.size());
+		
+			fabHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			fabHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	@Test
+	void testAllProducto() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+			assertEquals(11,listProd.size());
+		
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+
+	
+	}
+	
+	/**
+	 * 1. Lista los nombres y los precios de todos los productos de la tabla producto
+	 */
+	@Test
+	void test1() {
+	
+		ProductoHome prodHome = new ProductoHome();
+		
+		try {
+			prodHome.beginTransaction();
+			
+			List<Producto> listProd = prodHome.findAll();
+			
+			//TODO STREAMS
+	
+			
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	
+	}
+	
+	
+	/**
+	 * 2. Devuelve una lista de Producto completa con el precio de euros convertido a dólares .
+	 */
+	@Test
+	void test2() {
+		
+		ProductoHome prodHome = new ProductoHome();
+		
+		try {
+			prodHome.beginTransaction();			
+			List<Producto> listProd = prodHome.findAll();
+			
+			//TODO STREAMS
+			
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 3. Lista los nombres y los precios de todos los productos, convirtiendo los nombres a mayúscula.
+	 */
+	@Test
+	void test3() {
+		
+		
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+		
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 4. Lista el nombre de todos los fabricantes y a continuación en mayúsculas los dos primeros caracteres del nombre del fabricante.
+	 */
+	@Test
+	void test4() {
+	
+		FabricanteHome fabHome = new FabricanteHome();
+		
+		try {
+			fabHome.beginTransaction();
+	
+			List<Fabricante> listFab = fabHome.findAll();
+					
+			//TODO STREAMS
+					
+			fabHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			fabHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 * 5. Lista el código de los fabricantes que tienen productos.
+	 */
+	@Test
+	void test5() {
+	
+		FabricanteHome fabHome = new FabricanteHome();
+		
+		try {
+			fabHome.beginTransaction();
+	
+			List<Fabricante> listFab = fabHome.findAll();
+					
+			//TODO STREAMS
+		
+			fabHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			fabHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 * 6. Lista los nombres de los fabricantes ordenados de forma descendente.
+	 */
+	@Test
+	void test6() {
+	
+		FabricanteHome fabHome = new FabricanteHome();
+		
+		try {
+			fabHome.beginTransaction();
+	
+			List<Fabricante> listFab = fabHome.findAll();
+					
+			//TODO STREAMS
+		
+			fabHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			fabHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 * 7. Lista los nombres de los productos ordenados en primer lugar por el nombre de forma ascendente y en segundo lugar por el precio de forma descendente.
+	 */
+	@Test
+	void test7() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+			
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 8. Devuelve una lista con los 5 primeros fabricantes.
+	 */
+	@Test
+	void test8() {
+	
+		FabricanteHome fabHome = new FabricanteHome();
+		
+		try {
+			fabHome.beginTransaction();
+	
+			List<Fabricante> listFab = fabHome.findAll();
+					
+			//TODO STREAMS
+		
+			fabHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			fabHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 * 9.Devuelve una lista con 2 fabricantes a partir del cuarto fabricante. El cuarto fabricante también se debe incluir en la respuesta.
+	 */
+	@Test
+	void test9() {
+	
+		FabricanteHome fabHome = new FabricanteHome();
+		
+		try {
+			fabHome.beginTransaction();
+	
+			List<Fabricante> listFab = fabHome.findAll();
+					
+			//TODO STREAMS
+		
+			fabHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			fabHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+	}
+	
+	/**
+	 * 10. Lista el nombre y el precio del producto más barato
+	 */
+	@Test
+	void test10() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 11. Lista el nombre y el precio del producto más caro
+	 */
+	@Test
+	void test11() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+			
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 12. Lista el nombre de todos los productos del fabricante cuyo código de fabricante es igual a 2.
+	 * 
+	 */
+	@Test
+	void test12() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 13. Lista el nombre de los productos que tienen un precio menor o igual a 120€.
+	 */
+	@Test
+	void test13() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 14. Lista los productos que tienen un precio mayor o igual a 400€.
+	 */
+	@Test
+	void test14() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 15. Lista todos los productos que tengan un precio entre 80€ y 300€. 
+	 */
+	@Test
+	void test15() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();	
+			
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 16. Lista todos los productos que tengan un precio mayor que 200€ y que el código de fabricante sea igual a 6.
+	 */
+	@Test
+	void test16() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+						
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 17. Lista todos los productos donde el código de fabricante sea 1, 3 o 5 utilizando un Set de codigos de fabricantes para filtrar.
+	 */
+	@Test
+	void test17() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+			
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 18. Lista el nombre y el precio de los productos en céntimos.
+	 */
+	@Test
+	void test18() {
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();		
+			
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	
+	/**
+	 * 19. Lista los nombres de los fabricantes cuyo nombre empiece por la letra S
+	 */
+	@Test
+	void test19() {
+	
+	
+		FabricanteHome fabHome = new FabricanteHome();
+		
+		try {
+			fabHome.beginTransaction();
+	
+			List<Fabricante> listFab = fabHome.findAll();
+					
+			//TODO STREAMS
+		
+			fabHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			fabHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 20. Devuelve una lista con los productos que contienen la cadena Portátil en el nombre.
+	 */
+	@Test
+	void test20() {
+	
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();
+			
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 21. Devuelve una lista con el nombre de todos los productos que contienen la cadena Monitor en el nombre y tienen un precio inferior a 215 €.
+	 */
+	@Test
+	void test21() {
+	
+	
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();
+			
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+	/**
+	 * 22. Lista el nombre y el precio de todos los productos que tengan un precio mayor o igual a 180€. 
+	 * Ordene el resultado en primer lugar por el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente).
+	 */
+	void test22() {
+		
+		
+		ProductoHome prodHome = new ProductoHome();	
+		try {
+			prodHome.beginTransaction();
+		
+			List<Producto> listProd = prodHome.findAll();
+			
+			//TODO STREAMS
+				
+			prodHome.commitTransaction();
+		}
+		catch (RuntimeException e) {
+			prodHome.rollbackTransaction();
+		    throw e; // or display error message
+		}
+		
+	}
+	
+}
